10f79beb5c85fb1b4f6eae33c83c316082406e88
GunClayton <114902151+Smartsoil-Media@users.noreply.github.com>
2023-06-13 12:13:04 +0800

    initail commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const hero = {
+
+ 
+ const hero = {
    elementId: "hero",
    name: "Wizard",
    avatar: "/images/wizard.png",
    health: 60,
-   diceRoll: [3, 1, 4],
    diceCount: 3
 }
 
@@ -12,7 +13,6 @@ const monster = {
    name: "Orc",
    avatar: "/images/orc.png",
    health: 10,
-   diceRoll: [4],
    diceCount: 1
 }
 
@@ -21,37 +21,57 @@ const JMack = {
    name: "The Big Dog",
    avatar: "https://plus.unsplash.com/premium_photo-1682097238346-3f2a677ccfe6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2FycmlvcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
    health: 100,
-   diceRoll: [4,3,1,5],
    diceCount: 4
 }
 
+function Character(data) {
+   this.elementId = data.elementId
+   this.name = data.name
+   this.avatar = data.avatar
+   this.health = data.health
+   this.diceCount = data.diceCount
 
-function renderCharacter(data) {
-   const { elementId, name, avatar, health, diceRoll, diceCount} = data;
+   this.getCharacterHtml = function () {
+      const {name, avatar, health, diceCount } = this;
+      const diceHtml = getDiceHtml(diceCount)
 
-   let diceHtml = ""
+      document.getElementById(this.elementId).innerHTML =
+          `<div class="character-card">
+          <h4 class="name"> ${name} </h4>
+          <img class="avatar" src="${avatar}" />
+          <div class="health">health: <b> ${health} </b></div>
+          <div class="dice-container">    
+              ${diceHtml}
+          </div>
+      </div>`;
 
-   for (let i = 0; i < diceCount; i++) {
-      diceHtml += `<div class="dice">${diceRoll[i]}</div>`
-   }
+  }
 
-   document.getElementById(elementId).innerHTML += 
-      `
-      <div class="character-card">
-         <h4 class="name"> ${name} </h4>
-         <img class="avatar" src="${avatar}"/>
-         <p class="health">health: <b> ${health} </b></p>
-         <div class="dice-container">
-           ${diceHtml}
-         </div>
-      </div>        
-`
-   
+}
+const wizard = new Character(hero)
+const orc = new Character(monster)
+wizard.getCharacterHtml()
+orc.getCharacterHtml()
+
+
+function getDiceRollArray(diceCount) {
+   return new Array(diceCount).fill(0).map(function() {
+      return Math.floor(Math.random()*6) + 1
+  })
+
+
+}
+console.log(getDiceRollArray(3))
+
+function getDiceHtml(diceCount) {
+ return getDiceRollArray(diceCount).map(function(num){
+   return `<div class="dice">${num}</div>`
+ }).join('')
 }
 
 
+console.log(getDiceHtml(3))
 
 
-renderCharacter(hero)
-renderCharacter(monster)
+
 
